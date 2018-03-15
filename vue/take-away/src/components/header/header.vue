9b1de35c93f3f0bc1ae68b0383675013263e1ce0
fenpho <fenpho@163.com>
2018-03-15 23:26:37 +0800

    add take-away header background

--- vue/take-away/src/components/header/header.vue
+++ vue/take-away/src/components/header/header.vue
@@ -17,8 +17,17 @@
          <span class="text">{{seller.supports[0].description}}</span>
        </div>
      </div>
+     <div v-if="seller.supports" class="support-count">
+       <span class="count">{{seller.supports.length}}个</span>
+       <i class="icon-keyboard_arrow_right"></i>
+     </div>
    </div>
-   <div class="bulletin-wrapper"></div>
+   <div class="bulletin-wrapper">
+     <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span><i class="icon-keyboard_arrow_right"></i>
+   </div>
+   <div class="background">
+     <img :src="seller.avatar" alt="背景" width="100%" height="100%">
+   </div>
  </div>
 </template>
 
@@ -43,10 +52,12 @@ export default {
 @import '../../common/stylus/mixin';
 
 .header {
+  position: relative;
   color: #fff;
-  background: #000;
+  background: rgba(7, 17, 27, 0.5);
 
   .content-wrapper {
+    position: relative;
     padding: 24px 12px 18px 24px;
     font-size: 0;
 
@@ -123,10 +134,78 @@ export default {
 
         .text {
           line-height: 12px;
-          font-size: 12px;
+          font-size: 10px;
         }
       }
     }
+
+    .support-count {
+      position: absolute;
+      right: 12px;
+      bottom: 18px;
+      padding: 0 8px;
+      height: 24px;
+      line-height: 24px;
+      border-radius: 14px;
+      background: rgba(0, 0, 0, 0.2);
+      text-align: center;
+
+      .count {
+        vertical-align: top;
+        font-size: 10px;
+      }
+
+      .icon-keyboard_arrow_right {
+        margin-left: 2px;
+        line-height: 24px;
+        font-size: 10px;
+      }
+    }
+  }
+
+  .bulletin-wrapper {
+    position: relative;
+    height: 24px;
+    line-height: 28px;
+    padding: 0 22px 0 12px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    background: rgba(7, 17, 27, 0.2);
+
+    .bulletin-title {
+      display: inline-block;
+      vertical-align: top;
+      margin-top: 7px;
+      width: 22px;
+      height: 12px;
+      bg-image('bulletin');
+      background-size: 22px 12px;
+      background-repeat: no-repeat;
+    }
+
+    .bulletin-text {
+      vertical-align: top;
+      margin: 0 4px;
+      font-size: 10px;
+    }
+
+    .icon-keyboard_arrow_right {
+      position: absolute;
+      font-size: 10px;
+      right: 12px;
+      top: 8px;
+    }
+  }
+
+  .background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    filter: blur(10px);
   }
 }
 </style>
