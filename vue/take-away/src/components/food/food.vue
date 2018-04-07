0a220a1daa08bc00373297f4c796a853ff55f4d6
fenpho <fenpho@163.com>
2018-04-07 17:04:37 +0800

    finished goods page and good detail page for take-away

--- vue/take-away/src/components/food/food.vue
+++ vue/take-away/src/components/food/food.vue
@@ -39,7 +39,25 @@
               :desc="desc"
               :ratings="food.ratings"
               @select-type="select"
-              @toggle-content="toggle"></v-ratingselect>
+              @toggle-content="toggle">
+            </v-ratingselect>
+            <div class="rating-wrapper">
+              <ul v-show="food.ratings && food.ratings.length">
+                <li v-show="needShow(rating.rateType, rating.text)" class="rating-item border-1px" v-for="(rating, index) in food.ratings" :key="index">
+                  <div class="user">
+                    <span class="name">{{rating.username}}</span>
+                    <img class="avatar" :src="rating.avatar" alt="用户头像" width="12" height="12">
+                  </div>
+                  <div class="time">{{rating.rateTime | formatDate}}</div>
+                  <p class="text">
+                    <span :class="{'icon-thumb_up': rating.rateType === 0, 'icon-thumb_down': rating.rateType === 1}"></span>{{rating.text}}
+                  </p>
+                </li>
+              </ul>
+              <div class="no-rating" v-show="!food.ratings || !food.ratings.length">
+                暂无评价
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -50,6 +68,7 @@
 <script type='text/ecmascript-6'>
 import Vue from 'vue';
 import BScroll from 'better-scroll';
+import {formatDate} from '../../common/js/date';
 import cartcontrol from '@/components/cartcontrol/cartcontrol';
 import split from '@/components/split/split';
 import ratingSelect from '@/components/ratingSelect/ratingSelect';
@@ -80,6 +99,12 @@ export default {
     'v-cartcontrol': cartcontrol,
     'v-split': split,
     'v-ratingselect': ratingSelect
+  },
+  filters: {
+    formatDate(time) {
+      let date = new Date(time);
+      return formatDate(date, 'YYYY-MM-DD hh:mm');
+    }
   },
   methods: {
     show() {
@@ -120,12 +145,24 @@ export default {
       this.$nextTick(() => {
         this.scroll.refresh();
       });
+    },
+    needShow(type, text) {
+      if (this.onlyContent && !text) {
+        return false;
+      }
+      if (this.selectType === ALL) {
+        return true;
+      } else {
+        return type === this.selectType;
+      }
     }
   }
 };
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus'>
+@import '../../common/stylus/mixin';
+
 .food {
   position: fixed;
   left: 0;
@@ -275,6 +312,69 @@ export default {
       margin-bottom: 6px;
       font-size: 14px;
       color: rgb(7, 17, 27);
+    }
+
+    .rating-wrapper {
+      padding: 0 18px;
+
+      .rating-item {
+        position: relative;
+        padding: 16px 0;
+        border-1px(rgba(7, 17, 27, 0.1));
+
+        .user {
+          position: absolute;
+          right: 0;
+          top: 16px;
+          line-height: 16px;
+          font-size: 0;
+
+          .name {
+            display: inline-block;
+            margin-right: 6px;
+            vertical-align: top;
+            font-size: 10px;
+            color: rgb(147, 153, 159);
+          }
+
+          .avatar {
+            border-radius: 50%;
+          }
+        }
+
+        .time {
+          margin-bottom: 6px;
+          line-height: 12px;
+          font-size: 10px;
+          color: rgb(147, 153, 159);
+        }
+
+        .text {
+          line-height: 16px;
+          font-size: 12px;
+          color: rgb(7, 17, 27);
+
+          .icon-thumb_up, .icon-thumb_down {
+            margin-right: 4px;
+            line-height: 16px;
+            font-size: 12px;
+          }
+        }
+
+        .icon-thumb_up {
+          color: rgb(0, 160, 220);
+        }
+
+        .icon-thumb_down {
+          color: rgb(147, 153, 159);
+        }
+      }
+
+      .no-rating {
+        padding: 16px 0;
+        font-size: 12px;
+        color: rgb(147, 153, 159);
+      }
     }
   }
 }
--- vue/take-away/src/components/ratings/ratings.vue
+++ vue/take-away/src/components/ratings/ratings.vue
@@ -1,12 +1,26 @@
 <template>
- <div>
-        ratings
+ <div class="ratings">
+   <div class="ratings-content">
+     <div class="overview">
+       <div class="overview-left">
+         <h2 class="score">{{seller.score}}</h2>
+         <div class="title">综合评分</div>
+         <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+       </div>
+       <div class="overview-right"></div>
+     </div>
+   </div>
  </div>
 </template>
 
 <script type='text/ecmascript-6'>
 export default {
-  data () {
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data() {
     return {};
   },
   components: {}
@@ -14,4 +28,49 @@ export default {
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus'>
+.ratings {
+  position: absolute;
+  top: 174px;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+
+  .overview {
+    display: flex;
+    padding: 18px 0;
+
+    .overview-left {
+      flex: 0 0 137px;
+      width: 137px;
+      text-align: center;
+      border-right: 1px solid rgba(7, 17, 27, 0.1);
+
+      .score {
+        margin-bottom: 6px;
+        line-height: 28px;
+        font-size: 24px;
+        color: rgb(255, 153, 0);
+      }
+
+      .title {
+        margin-bottom: 8px;
+        line-height: 12px;
+        font-size: 12px;
+        color: rgb(7, 17, 27);
+      }
+
+      .rank {
+        line-height: 10px;
+        font-size: 10px;
+        color: rgb(147, 153, 159);
+      }
+    }
+  }
+
+  .overview-right {
+    flex: 1;
+    padding-left: 24px;
+  }
+}
 </style>
