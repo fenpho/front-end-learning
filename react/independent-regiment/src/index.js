f6be96862ef25ec98db5f8c7ee5c59e38b67221f
fenpho <fenpho@163.com>
2018-05-27 11:21:44 +0800

    simple use react-router for independent-regiment

--- react/independent-regiment/src/index.js
+++ react/independent-regiment/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import App from './App';
 import { counter } from './index.redux';
@@ -12,9 +13,33 @@ const store = createStore(counter, compose(
     window.devToolsExtension ? window.devToolsExtension() : () => { }
 ));
 
+function Erying() {
+    return <h2>二营</h2>
+}
+function Qibinglian() {
+    return <h2>骑兵连</h2>
+}
+
 ReactDOM.render(
     (<Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <div>
+                <ul>
+                    <li>
+                        <Link to='/'>一营</Link>
+                    </li>
+                    <li>
+                        <Link to='/erying'>二营</Link>
+                    </li>
+                    <li>
+                        <Link to='/qibinglian'>骑兵连</Link>
+                    </li>
+                </ul>
+                <Route path='/' exact component={App}></Route>
+                <Route path='/erying' component={Erying}></Route>
+                <Route path='/qibinglian' component={Qibinglian}></Route>
+            </div>
+        </BrowserRouter>
     </Provider>),
     document.getElementById('root')
 );
