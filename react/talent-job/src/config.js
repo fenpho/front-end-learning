74d5cc67291ea828a09349347281301954bf295a
fenpho <fenpho@163.com>
2018-05-30 00:34:01 +0800

    init talent-job-1

--- react/talent-job/src/config.js
+++ react/talent-job/src/config.js
@@ -2,14 +2,12 @@ import axios from 'axios';
 import { Toast } from 'antd-mobile';
 
 // 拦截请求
-axios.interceptors.request.use(function(config) {
+axios.interceptors.request.use(function (config) {
     Toast.loading('加载中', 0);
     return config;
 })
 // 拦截响应
-axios.interceptors.response.use(function(config) {
-    setTimeout(() => {
-        Toast.hide();        
-    }, 2000);
+axios.interceptors.response.use(function (config) {
+    Toast.hide();
     return config;
 })
