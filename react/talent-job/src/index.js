4bb5972fddcd21fb12f50547c36dcfdbf6aa1d17
fenpho <fenpho@163.com>
2018-05-29 23:18:37 +0800

    add init project file for talent-job

--- react/talent-job/src/index.js
+++ react/talent-job/src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { 
+    BrowserRouter, 
+    Route, 
+    Switch, 
+    Redirect 
+} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducer from './reducer'
+import './config';
+
+const store = createStore(reducer, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : () => { }
+));
+
+// 登录
+// 没有登录信息，统一跳转到login
+// 页面 导航+显示+注销
+// 一营
+// 二营
+// 骑兵连
+// router+redux
+
+ReactDOM.render(
+    (<Provider store={store}>
+        <BrowserRouter>
+        </BrowserRouter>
+    </Provider>),
+    document.getElementById('root')
+);
---
+++ react/talent-job/src/reducer.js
@@ -0,0 +1,5 @@
+// 合并多个reducer
+import { combineReducers } from 'redux';
+
+export default combineReducers({});
+
